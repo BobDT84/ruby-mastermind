e6dd784ac57526acd4a8b9af58e98e2556d3b3df
Bob <bobdt84@gmail.com>
2022-05-01 01:49:00 -0500

    Added methods process_guess, and play_round.  The logic for process guess needs works

--- mastermind.rb
+++ mastermind.rb
@@ -9,6 +9,7 @@ class Mastermind
     @guess_history = []
     @hint_history = []
     test
+    play_round
   end
 
   private
@@ -19,28 +20,52 @@ class Mastermind
     code
   end
 
+  def play_round
+    print_history unless @guess_history.empty?
+    puts "Round #{@round + 1} of 12"
+    get_player_guess
+    process_guess
+    next_round
+  end
+
   def get_player_guess
     guess = []
     4.times do |i|
       guess.push(ask_and_check("Guess Color#{i+1}: ", :check_color, [@colors]))
     end
-    guess
+    @guess_history.push(guess)
+  end
+
+  def process_guess
+    hint = []
+    guess = @guess_history[@round]
+    4.times do |i|
+      if guess[i] == @secret_code[i]
+        hint.push('H')
+      elsif @secret_code.include?(guess[i])
+        hint.push('m')
+      end
+    end
+    @hint_history.push(hint)
   end
 
   def print_history
-    0...@round.each do |i|
-      puts @guess_history[i] + @hint_history[i]
+    (0...@round).each do |i|
+      puts "Round#{i+1}: " + @guess_history[i].join(', ') + ' => ' + @hint_history[i].join(', ')
     end
   end
 
   def next_round
     @round += 1
+    if @round < 12
+      play_round
+    else
+      game_over
+    end
   end
 
   def test
-    p @secret_code
-    p @colors
-    p get_player_guess
+    p "Secret Code: #{@secret_code}"
   end
 end
 
